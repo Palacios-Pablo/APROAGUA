f931291adb2766910cd745ccb2ddb6597488451d
Pablo-Palacios <pjpr.16@hotmail.com>
2024-10-23 11:32:03 -0600

    cambio de config.js

--- aproagua-frontend/src/config.js
+++ aproagua-frontend/src/config.js
@@ -1,7 +1,7 @@
 // config.js
 const config = {
     API_BASE_URL: process.env.NODE_ENV === 'production'
-      ? 'https://aproagua-backend-6d44ee3f85f0.herokuapp.com/'  // URL en producción
+      ? 'https://aproagua-backend-6d44ee3f85f0.herokuapp.com'  // URL en producción
       : 'http://localhost:3000',  // URL en desarrollo local
   };
   
